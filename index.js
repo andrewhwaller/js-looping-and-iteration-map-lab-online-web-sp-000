89b92188e809bcce61a6123065d60aab7b13d0a6
Andrew Waller <andrewhwaller@gmail.com>
2019-09-09 21:42:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ function lowerCaseDrivers(drivers) {
     return driver.toLowerCase();
   });
 }
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(driver) {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+    return { firstName: driverFirst; lastName: driverLast };
+  }):
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+}
