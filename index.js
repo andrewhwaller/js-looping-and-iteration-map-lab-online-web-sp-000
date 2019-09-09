1b01508745e9b1a4a1c49cf22e811638db6d651f
Andrew Waller <andrewhwaller@gmail.com>
2019-09-09 21:37:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
-  return drivers.map(driver)
+  return drivers.map(function(driver) {
+    return driver.toLowerCase();
+  });
 
 }
